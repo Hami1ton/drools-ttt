9e84f6bbeccc615230b23bb1bf65efcf064c981d
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2025-08-10 00:39:51 +0900

    勝敗判定できるところまで

--- drools-uttt/src/main/java/org/example/drools/uttt/model/Mark.java
+++ drools-uttt/src/main/java/org/example/drools/uttt/model/Mark.java
@@ -1,3 +1,30 @@
 package org.example.drools.uttt.model;
 
-public record Mark(int row, int col, String mark){}
+public class Mark {
+
+    public final int row;
+
+    public final int col;
+
+    public final String mark;
+
+    public final int globalRow;
+
+    public final int globalCol;
+
+    public final int localRow;
+
+    public final int localCol;
+
+    public Mark(int row, int col, String mark) {
+        this.row = row;
+        this.col = col;
+        this.mark = mark;
+
+        this.globalRow = row / 3;
+        this.globalCol = row / 3;
+        this.localRow = row % 3;
+        this.localCol = row % 3;
+    }
+
+}
